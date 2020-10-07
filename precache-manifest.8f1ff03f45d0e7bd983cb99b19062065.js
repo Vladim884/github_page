0ba42188c4c9f0bafa407345ca6bd2426d4f0457
vladim <vladim.volovenko@gmail.com.com>
2020-10-07 10:41:27 +0300

    Updates

---
+++ precache-manifest.8f1ff03f45d0e7bd983cb99b19062065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f4e3349e056c4a6a126211f8462609",
+    "url": "/github_page/index.html"
+  },
+  {
+    "revision": "f4d6ca99de435b6efa01",
+    "url": "/github_page/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "48d1738bcc9ff7986e32",
+    "url": "/github_page/static/js/2.77a8b067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_page/static/js/2.77a8b067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d6ca99de435b6efa01",
+    "url": "/github_page/static/js/main.677efed8.chunk.js"
+  },
+  {
+    "revision": "9e3568b50666a70a310b",
+    "url": "/github_page/static/js/runtime-main.2a0e80f7.js"
+  },
+  {
+    "revision": "9c6eca1496f34caeedd7d7012efecd6d",
+    "url": "/github_page/static/media/my_photo.9c6eca14.jpg"
+  }
+]);
