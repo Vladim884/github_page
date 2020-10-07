9fcc2df131e2e11904689e0caea46d10c652953d
vladim <vladim.volovenko@gmail.com.com>
2020-10-07 10:39:47 +0300

    deploy my react-app to github-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profile_photo from './img/my_photo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React-App</h1>
+      <h2>Author: Vladimir Volovenko</h2>
+      <img src={profile_photo} alt="profile-image" / >
     </div>
   );
 }
